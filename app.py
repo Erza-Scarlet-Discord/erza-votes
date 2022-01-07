488b6766e267d70ca07e30bc5e32a3322b06a143
ATRS <82661604+ATRS7391@users.noreply.github.com>
2022-01-07 16:21:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import requests
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['JSON_SORT_KEYS'] = False
+
+
+@app.route('/', methods=['POST'])
+def index():
+    if request.method == "POST":
+        data = request.get_json()
+        main = requests.get("https://erza-ai.herokuapp.com")
+        backup = requests.get("https://erza-ai-backup.herokuapp.com")
+        es26 = requests.get("https://erza-scarlet-26.herokuapp.com")
+        api = None
+        if main.status_code == 200:
+            print("erza-ai is up and running")
+            api = "https://erza-ai.herokuapp.com/"
+        elif backup.status_code == 200:
+            print("erza-ai-backup is up and running")
+            api = "https://erza-ai-backup.herokuapp.com/"
+        elif es26.status_code == 200:
+            print("es26 is up and running")
+            api = "https://erza-scarlet-26.herokuapp.com/"
+        else:
+            print("[w] : none of them are up and running")
+            print(es26, main, backup)
+
+        if api:
+            requests.post(api + "api/v2/vote", json=data)
+
+
+if __name__ == '__main__':
+    app.run()
