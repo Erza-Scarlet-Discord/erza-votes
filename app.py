8034d6791fc590da8cc52f8e516e6fdd16ac2196
ATRS <82661604+ATRS7391@users.noreply.github.com>
2022-01-08 11:36:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -12,6 +13,9 @@ app.config['JSON_SORT_KEYS'] = False
 def index():
     if request.method == "POST":
         data = request.get_json()
+        headers = request.headers
+        print(headers)
+        print(data)
         main = requests.get("https://erza-ai.herokuapp.com")
         backup = requests.get("https://erza-ai-backup.herokuapp.com")
         es26 = requests.get("https://erza-scarlet-26.herokuapp.com")
@@ -30,7 +34,7 @@ def index():
             print(es26, main, backup)
 
         if api:
-            requests.post(api + "api/v2/votes", json=data, headers=request.headers)
+            requests.post(api + "api/v2/votes", json=json.dumps(data), headers=request.headers)
             print("posted")
             
         return jsonify(code="working", message=f"POST to {api}"), 200
